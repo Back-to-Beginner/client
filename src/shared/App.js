f878936fd35de48733c1aa6057b2f120722d3030
Daehyeon98 <101495251+Daehyeon98@users.noreply.github.com>
2022-05-26 14:12:33 +0900

    Update App.js

--- src/shared/App.js
+++ src/shared/App.js
@@ -8,7 +8,11 @@ import { history } from "../redux/configureStore";
 import Header from "../components/Header";
 import Main from "../pages/Main";
 import PostList from "../pages/PostList";
+import ChangeInfo from "../pages/ChangeInfo";
 import PostView from "../pages/PostView";
+import PostMain from "../pages/PostMain";
+import Withdraw from "../pages/Withdraw";
+import PostUp from "../pages/PostUp";
 import PostDetail from "../pages/PostDetail";
 import Login from "../pages/Login";
 import Mypage from "../pages/Mypage";
@@ -23,8 +27,12 @@ function App() {
       <Header></Header>
       <ConnectedRouter history={history}>
         <Route path="/" exact component={Main} />
-        <Route path="/Postlist" exact component={PostList} />
-        <Route path="/PostView" exact component={PostView} />
+        <Route path="/Postlist" exact component={PostList} />  
+        <Route path="/PostMain" exact component={PostMain} />
+        <Route path="/Withdraw" exact component={Withdraw} />
+        <Route path="/ChangeInfo" exact component={ChangeInfo} />
+        <Route path="/PostUp" exact component={PostUp} />
+        <Route path="/PostView/:no" exact component={PostView} />
         <Route path="/mypage" exact component={Mypage} />
         <Route path="/login" exact component={Login} />
         <Route path="/signup" exact component={Signup} />
