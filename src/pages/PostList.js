9ef6e5c147e7f8b61b40489b556547b65966bc85
Daehyeon98 <101495251+Daehyeon98@users.noreply.github.com>
2022-06-09 17:39:59 +0900

    Update PostList.js

--- src/pages/PostList.js
+++ src/pages/PostList.js
@@ -18,8 +18,8 @@ const PostList = props => {
   // const [{}, setData] = React.useState("");
   // const [time, setTimestamp] = React.useState("");
   const dispatch = useDispatch();
-  const post_list = useSelector((state) => state.post.list);
-  const together = post_list.slice(0, 101);
+  // const post_list = useSelector((state) => state.post.list);
+  
 
   // const changeCode = (e) => {
   //   console.log(e.target.value);
@@ -67,72 +67,11 @@ const PostList = props => {
           }) : ''
         }
       </CommonTable>
-      <div
-        style={{
-          width: "980px",
-          display: "flex",
-          justifyContent: "space-around",
-          flexWrap: "wrap",
-          margin: "0px auto",
-        }}
-      >
-        {together.map((p, idx) => {
-          const momentId = p.momentId;
-          return (
-            <div
-              style={{
-                width: "calc(25% - 44px)",
-                marginRight: "44px",
-                marginBottom: "56px",
-              }}
-              onClick={() => {
-                history.push(`/detail/${momentId}`);
-              }}
-            >
-              <Post key={p.momentId} {...p} />
-            </div>
-          );
-        })}  
-        </div>
-    </>
-  )
-}
+      </>    
+ ) 
+} 
 
-export default PostList;
-// const PostList = () => {
-//   const dispatch = useDispatch();
-//   const post_list = useSelector((state) => state.post.list);
-
-//   const together = post_list.slice(0, 101);
-
-//   React.useEffect(() => {
-//     window.scrollTo(0, 0);
-
-//     dispatch(postActions.getPostDB());
-//   }, []);
-
-//   return (
-//     <React.Fragment>
-//       <div></div>
-//       <h1 style={{ textAlign: "center", fontSize: "38px", marginTop: "50px" }}>
-//        사람들의 다양한 여정
-//       </h1>
-
-//       <div
-//         style={{
-//           width: "890px",
-//           display: "flex",
-//           justifyContent: "flex-end",
-//           margin: "0px auto",
-//           marginBottom: "30px",
-//         }}
-//       ></div>
 
 
 
-      
-//     </React.Fragment>
-//   );
-// };
-
-// export default PostList;
+export default PostList;
